230751800d6ab793beeb2fd8110e59fe0a30d84a
Simon Larsson <snigelson@gmail.com>
2013-07-26 15:17:52 +0200

    [gpio] Added functions to handle interrupts
    The functions (Enable|Disable)GpioInterrupt to choose event and
    ClearGpioInterrupt to acknowledge an event.
    
    Filled out the rest of the register struct.

--- Demo/Drivers/gpio.h
+++ Demo/Drivers/gpio.h
@@ -7,9 +7,16 @@ void SetGpio			(unsigned int pinNum, unsigned int pinVal);
 
 
 
+enum DETECT_TYPE {
+	DETECT_NONE,
+	DETECT_RISING,
+	DETECT_FALLING
+};
 
+void EnableGpioDetect(unsigned int pinNum, enum DETECT_TYPE type);
+void DisableGpioDetect(unsigned int pinNum, enum DETECT_TYPE type);
 
-
+void ClearGpioInterrupt(unsigned int pinNum);
 
 
 

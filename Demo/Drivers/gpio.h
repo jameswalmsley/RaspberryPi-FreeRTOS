1bbefe39b9964c2e653532e5b2d78e424e74493c
Simon Larsson <snigelson@gmail.com>
2013-07-30 13:43:55 +0200

    [gpio] Added detect events and functions
    Added detect events for high/low and asynchronous rising/falling
    
    Added the wrapper SetGpioDirection and a function to read pins.

--- Demo/Drivers/gpio.h
+++ Demo/Drivers/gpio.h
@@ -1,22 +1,49 @@
 #ifndef _GPIO_H_
 #define _GPIO_H_
 
-
-void SetGpioFunction	(unsigned int pinNum, unsigned int funcNum);
-void SetGpio			(unsigned int pinNum, unsigned int pinVal);
-
-
-
+/* GPIO event detect types */
 enum DETECT_TYPE {
 	DETECT_NONE,
 	DETECT_RISING,
-	DETECT_FALLING
+	DETECT_FALLING,
+	DETECT_HIGH,
+	DETECT_LOW,
+	DETECT_RISING_ASYNC,
+	DETECT_FALLING_ASYNC
 };
 
-void EnableGpioDetect(unsigned int pinNum, enum DETECT_TYPE type);
-void DisableGpioDetect(unsigned int pinNum, enum DETECT_TYPE type);
+/* GPIO pull up or down states */
+enum PULL_STATE {
+	PULL_DISABLE,
+	PULL_UP,
+	PULL_DOWN,
+	PULL_RESERVED
+};
 
-void ClearGpioInterrupt(unsigned int pinNum);
+/* Pin data direction */
+enum GPIO_DIR {
+	GPIO_IN,
+	GPIO_OUT
+};
+
+/* GPIO pin setup */
+void SetGpioFunction	(unsigned int pinNum, unsigned int funcNum);
+/* A simple wrapper around SetGpioFunction */
+void SetGpioDirection	(unsigned int pinNum, enum GPIO_DIR dir);
+
+/* Set GPIO output level */
+void SetGpio			(unsigned int pinNum, unsigned int pinVal);
+
+/* Read GPIO pin level */
+int ReadGpio			(unsigned int pinNum);
+
+/* GPIO pull up/down resistor control function */
+int PudGpio				(unsigned int pinNum, enum PULL_STATE state);
+
+/* Interrupt related functions */
+void EnableGpioDetect	(unsigned int pinNum, enum DETECT_TYPE type);
+void DisableGpioDetect	(unsigned int pinNum, enum DETECT_TYPE type);
+void ClearGpioInterrupt	(unsigned int pinNum);
 
 
 
